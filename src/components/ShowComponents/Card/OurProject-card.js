f0b0c29c00bdb0b9bfd3e89e9c630e568f1fc7ab
panupongjkn <panupong.jkn@gmail.com>
2020-07-11 00:01:21 +0700

    [+] create file json

--- src/components/ShowComponents/Card/OurProject-card.js
+++ src/components/ShowComponents/Card/OurProject-card.js
@@ -1,15 +1,24 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 function OurProjectCard(props) {
   return (
     <div {...props}>
       <div class="card" style={{ width: "100%" }}>
-        <img src={props.image} class="card-img-top" alt={props.title} />
+        <img
+          src={`/images/projects/${props.image}`}
+          class="card-img-top"
+          alt={props.title}
+        />
         <div class="card-body">
           <h5 class="card-title">{props.title}</h5>
-          <p class="card-text">{props.children}</p>
+          <p class="card-text">
+            {props.children}{" "}
+          </p>
           <div className="text-center">
-            <button class="btn btn-dark">ข้อมูลเพิ่มเติม</button>
+            <Link to={`/project/${props.id}`}>
+              <button class="btn btn-dark">ข้อมูลเพิ่มเติม</button>
+            </Link>
           </div>
         </div>
       </div>
--- src/components/ShowComponents/ShowOurProject.js
+++ src/components/ShowComponents/ShowOurProject.js
@@ -1,37 +1,22 @@
 import React from "react";
 import OurProjectCard from "./Card/OurProject-card";
-import Asset01 from "../../assest/Asset-01.jpg";
 
-const data = [
-  {
-    image: Asset01,
-    title: "โครงการ 1",
-    content:
-      "เตรียมพบคอนโดใหม่ จากแสนสิริ ดีคอนโด บลิซ ให้ชีวิตรีแลกซ์ในแบบรีสอร์ท กับทำเลติด ม.เกษตร ศรีราชา ใกล้นิคมฯ จองวันนี้ ฟรีโอน และส่วนกลาง 2ปี*",
-  },
-  {
-    image: Asset01,
-    title: "โครงการ 2",
-    content:
-      "เตรียมพบคอนโดใหม่ จากแสนสิริ ดีคอนโด บลิซ ให้ชีวิตรีแลกซ์ในแบบรีสอร์ท กับทำเลติด ม.เกษตร ศรีราชา ใกล้นิคมฯ จองวันนี้ ฟรีโอน และส่วนกลาง 2ปี*",
-  },
-  {
-    image: Asset01,
-    title: "โครงการ 3",
-    content:
-      "เตรียมพบคอนโดใหม่ จากแสนสิริ ดีคอนโด บลิซ ให้ชีวิตรีแลกซ์ในแบบรีสอร์ท กับทำเลติด ม.เกษตร ศรีราชา ใกล้นิคมฯ จองวันนี้ ฟรีโอน และส่วนกลาง 2ปี*",
-  },
-];
+import data from "../../json/projects.json"
 
 function ShowOurProject() {
   return (
     <div>
       <h4>โปรเจคที่เคยทำ</h4>
-      <div className="col-12 pt-sm-3 px-0">
+      <div className="col-12 px-0">
         <div className="row">
           {data.map((condo, key) => {
             return (
-              <OurProjectCard className="col-12 col-sm-6 col-lg-4 pt-3 pt-lg-0" image={condo.image} title={condo.title}>
+              <OurProjectCard
+                className="col-12 col-sm-6 col-lg-4 pt-3"
+                image={`${condo.id}/${condo.imagecover}`}
+                title={condo.title}
+                id={condo.id}
+              >
                 <p>{condo.content}</p>
               </OurProjectCard>
             );
