467b09e2cb8cac700c18fdba97d225b3bb8960db
panupongjkn <panupong.jkn@gmail.com>
2020-07-10 22:39:39 +0700

    [+] update footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,66 +1,43 @@
-import React from 'react'
+import React from "react";
+import Member1 from "../assest/Panupong.jpg";
+import Member2 from "../assest/Asset-01.jpg";
 
+const team = [
+  {
+    image: Member1,
+    name: "Panupong Joknoi",
+    position: "Web Developer",
+  },
+  {
+    image: Member2,
+    name: "Natchanon Niwedtongrirk",
+    position: "Web Developer",
+  },
+  {
+    image: Member2,
+    name: "Chutikan Khuanpan",
+    position: "Web Designer",
+  },
+];
 function Footer() {
-    return (
-        <footer class="pt-4 my-md-5 pt-md-5 border-top">
-            <div className="row">
-                <div className="col-12 col-md">
-                    <h5>First</h5>
-                </div>
-                <div className="col-6 col-md">
-                    <h5>Second</h5>
-                    <ul className="list-unstyled text-small">
-                        <li>
-                            <a className="text-muted" href="#">Lorem</a>
-                        </li>
-                        <li>
-                            <a className="text-muted" href="#">Lorem</a>
-                        </li>
-                        <li>
-                            <a className="text-muted" href="#">Lorem</a>
-                        </li>
-                        <li>
-                            <a className="text-muted" href="#">Lorem</a>
-                        </li>
-                    </ul>
-                </div>
-                <div className="col-6 col-md">
-                    <h5>Thrid</h5>
-                    <ul className="list-unstyled text-small">
-                        <li>
-                            <a className="text-muted" href="#">Lorem</a>
-                        </li>
-                        <li>
-                            <a className="text-muted" href="#">Lorem</a>
-                        </li>
-                        <li>
-                            <a className="text-muted" href="#">Lorem</a>
-                        </li>
-                        <li>
-                            <a className="text-muted" href="#">Lorem</a>
-                        </li>
-                    </ul>
-                </div>
-                <div className="col-6 col-md">
-                    <h5>Fourth</h5>
-                    <ul className="list-unstyled text-small">
-                        <li>
-                            <a className="text-muted" href="#">Lorem</a>
-                        </li>
-                        <li>
-                            <a className="text-muted" href="#">Lorem</a>
-                        </li>
-                        <li>
-                            <a className="text-muted" href="#">Lorem</a>
-                        </li>
-                        <li>
-                            <a className="text-muted" href="#">Lorem</a>
-                        </li>
-                    </ul>
-                </div>
+  return (
+    <footer className="pt-4 my-md-5 pt-md-5 border-top ">
+      <div className="text-center">
+        <h4>Team</h4>
+      </div>
+      <div className="row pt-0 pt-sm-3">
+        {team.map((member, key) => {
+          return (
+            <div key={key} className="col-12 col-sm-4 pt-3 pt-sm-0 text-center">
+              <img src={member.image} style={{width:"150px", height:"150px", borderRadius:"50%"}} />
+              <p className="pt-4">{member.name}</p>
+              <p><b>Position:</b> {member.position}</p>
             </div>
-        </footer>
-    )
+          );
+        })}
+      </div>
+    </footer>
+  );
 }
 
 export default Footer;
