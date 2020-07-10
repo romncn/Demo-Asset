cfd86569487c82986ba6809ea24f22265fc66f8f
panupongjkn <panupong.jkn@gmail.com>
2020-07-10 22:40:37 +0700

    [+] update project card

--- src/components/ShowComponents/Card/OurProject-card.js
+++ src/components/ShowComponents/Card/OurProject-card.js
@@ -1,11 +1,20 @@
-import React from 'react'
+import React from "react";
 
-function OurProjectCard() {
-    return (
-        <div>
-           <h1>OurProjectCard</h1>
+function OurProjectCard(props) {
+  return (
+    <div {...props}>
+      <div class="card" style={{ width: "100%" }}>
+        <img src={props.image} class="card-img-top" alt={props.title} />
+        <div class="card-body">
+          <h5 class="card-title">{props.title}</h5>
+          <p class="card-text">{props.children}</p>
+          <div className="text-center">
+            <button class="btn btn-dark">ข้อมูลเพิ่มเติม</button>
+          </div>
         </div>
-    )
+      </div>
+    </div>
+  );
 }
 
 export default OurProjectCard;
--- src/components/ShowComponents/ShowOurProject.js
+++ src/components/ShowComponents/ShowOurProject.js
@@ -1,13 +1,45 @@
-import React from 'react'
-import OurProjectCard from './Card/OurProject-card'
+import React from "react";
+import OurProjectCard from "./Card/OurProject-card";
+import Asset01 from "../../assest/Asset-01.jpg";
+
+const data = [
+  {
+    image: Asset01,
+    title: "โครงการ 1",
+    content:
+      "เตรียมพบคอนโดใหม่ จากแสนสิริ ดีคอนโด บลิซ ให้ชีวิตรีแลกซ์ในแบบรีสอร์ท กับทำเลติด ม.เกษตร ศรีราชา ใกล้นิคมฯ จองวันนี้ ฟรีโอน และส่วนกลาง 2ปี*",
+  },
+  {
+    image: Asset01,
+    title: "โครงการ 2",
+    content:
+      "เตรียมพบคอนโดใหม่ จากแสนสิริ ดีคอนโด บลิซ ให้ชีวิตรีแลกซ์ในแบบรีสอร์ท กับทำเลติด ม.เกษตร ศรีราชา ใกล้นิคมฯ จองวันนี้ ฟรีโอน และส่วนกลาง 2ปี*",
+  },
+  {
+    image: Asset01,
+    title: "โครงการ 3",
+    content:
+      "เตรียมพบคอนโดใหม่ จากแสนสิริ ดีคอนโด บลิซ ให้ชีวิตรีแลกซ์ในแบบรีสอร์ท กับทำเลติด ม.เกษตร ศรีราชา ใกล้นิคมฯ จองวันนี้ ฟรีโอน และส่วนกลาง 2ปี*",
+  },
+];
 
 function ShowOurProject() {
-    return (
-        <div>
-           <h1>ShowOurProject</h1>
-           <OurProjectCard></OurProjectCard>
+  return (
+    <div>
+      <h4>โปรเจคที่เคยทำ</h4>
+      <div className="col-12 pt-sm-3 px-0">
+        <div className="row">
+          {data.map((condo, key) => {
+            return (
+              <OurProjectCard className="col-12 col-sm-6 col-lg-4 pt-3 pt-lg-0" image={condo.image} title={condo.title}>
+                <p>{condo.content}</p>
+              </OurProjectCard>
+            );
+          })}
         </div>
-    )
+      </div>
+    </div>
+  );
 }
 
 export default ShowOurProject;
