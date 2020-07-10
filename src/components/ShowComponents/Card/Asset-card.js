ec8d00165c00eae90f9771d878f0bc044ebbdf4f
panupongjkn <panupong.jkn@gmail.com>
2020-07-10 21:56:05 +0700

    [+] update assets card

--- src/components/ShowComponents/Card/Asset-card.js
+++ src/components/ShowComponents/Card/Asset-card.js
@@ -3,16 +3,14 @@ import React from "react";
 function AssetCard(props) {
   return (
     <div {...props}>
-      <div class="card" style={{ width: "18rem" }}>
+      <div class="card" style={{ width: "100%" }}>
         <img src={props.image} class="card-img-top" alt={props.title} />
         <div class="card-body">
           <h5 class="card-title">{props.title}</h5>
-          <p class="card-text">
-            {props.children}
-          </p>
-          <a href="#" class="btn btn-primary">
-            Go somewhere
-          </a>
+          <p class="card-text">{props.children}</p>
+          <div className="text-center">
+            <button class="btn btn-dark">ข้อมูลเพิ่มเติม</button>
+          </div>
         </div>
       </div>
     </div>
